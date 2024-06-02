d4eb8f99cdadee388d80fb935f61e4ccc27f4f64
satish9177 <satishkommanaboina@gmail.com>
2024-06-02 17:49:38 +0530

    cors is added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app=express();
 const PORT=process.env.PORT || 4000;
 const mongoose=require('mongoose')
 const connectDB=require('./config/configDB');
+const cors=require('cors')
 const hoteldataAddedintoDB=require('./Router/hotel.route')
 const hotelgetdata=require('./Router/hoteldataget.route')
 const categoryAddedintoDB=require('./Router/category.route')
@@ -11,6 +12,7 @@ const singlehotel=require('./Router/siglehotel.router')
 const signup=require('./Router/signup.router')
 const wishlist=require('./Router/wishlist.route')
 app.use(express.json())
+app.use(cors())
 connectDB();
 app.get('/',(req,res)=>{
    res.send("hello satish")
@@ -19,7 +21,7 @@ app.get('/',(req,res)=>{
 app.use('/api/hotelget',hotelgetdata);
 app.use('/api/hoteldata',hoteldataAddedintoDB);
 app.use("/api/categorydata",categoryAddedintoDB)
-app.use("/api/categoryget",categorygetdata)
+app.use("/api/categoryget",categorygetdata)+
 app.use('/api/hotelget',singlehotel)
 app.use('/api/auth',signup)
 app.use('/api/wishlist',wishlist)
